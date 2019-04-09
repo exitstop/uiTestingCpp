63d3d00036a7905c5c645c1dabc2d86fb8e8fa43
exitstop <exitstop@list.ru>
2019-04-09 14:20:57 +0300

    contextMenu.ps1

---
+++ Clicker/contextMenu.ps1
@@ -0,0 +1,5 @@
+$o = new-object -com Shell.Application
+$folder = $o.NameSpace("C:\Users\user\source\repos\Clicker\x64\Debug")
+$file=$folder.ParseName("Clicker.ilk")
+$file.Verbs() | select Name
+$file.Verbs() | %{ if($_.Name -eq 'Scan for_Security Threats') { $_.DoIt() } }
