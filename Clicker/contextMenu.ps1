e3f49a85c787748022363142ec7601aae888a1d0
exitstop <exitstop@list.ru>
2019-04-09 16:20:24 +0300

    chage context menu name

--- Clicker/contextMenu.ps1
+++ Clicker/contextMenu.ps1
@@ -5,4 +5,4 @@ $o = new-object -com Shell.Application
 $folder = $o.NameSpace("C:\Users\user\source\repos\Clicker\Clicker")
 $file=$folder.ParseName("CMakeLists.txt")
 $file.Verbs() | select Name
-$file.Verbs() | %{ if($_.Name -eq 'Scan for_Security Threats') { $_.DoIt() } }
+$file.Verbs() | %{ if($_.Name -eq 'Scan for Security Threats') { $_.DoIt() } }
